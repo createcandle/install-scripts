8aa3e95f8210ec9b83f0781abe9ef8ec8b06e18b
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-05-19 22:04:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 
 
 # MANAGE INSTALLED APPLICATIONS
+apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xserver-xorg-legacy xinit openbox wmctrl xdotool feh omxplayer fbi unclutter lsb-release xfonts-base libinput-tools nbtscan -y
 
-apt-get install fbi nbtscan openbox
+
 
 # TODO: chromium browser v88
-
+ 
 
 # Just to be safe, although it should already be installed:
 apt-get install omxplayer
@@ -65,6 +66,9 @@ systemctl enable candle_first_run.service
 systemctl enable candle_bootup_actions.service
 systemctl enable candle_start_swap.service
 
+systemctl enable splashscreen.service
+systemctl enable splashscreen180.service
+
 systemctl enable candle_hostname_fix.service # ugly solution, might not even be necessary anymore?
 
 
@@ -101,16 +105,69 @@ wget https://www.candlesmarthome.com/tools/splash.mp4 -P /boot/
 mkdir -p /usr/share/plymouth/themes/pix/
 cp /boot/splash.png /usr/share/plymouth/themes/pix/splash.png
 
+# Hides the Raspberry Pi logos shown at boot
+isInFile2=$(cat /boot/config.txt | grep -c "disable_splash")
+if [ $isInFile2 -eq 0 ]
+then
+	echo "- Adding disable_splash to config.txt"
+	echo 'disable_splash=1' >> /boot/config.txt
+else
+    echo "- Splash was already disabled in config.txt"
+fi
+
+# Hide the text normally shown when linux boots up
+isInFile=$(cat /boot/cmdline.txt | grep -c "tty3")
+if [ $isInFile -eq 0 ]
+then    
+	echo "- Modifying cmdline.txt"
+	# change text output to third console. press alt-shift-F3 during boot to see it again.
+    sed -i 's/tty1/tty3/' /boot/cmdline.txt
+	# hide all the small things normally shown at boot
+	sed -i ' 1 s/.*/& quiet plymouth.ignore-serial-consoles splash logo.nologo vt.global_cursor_default=0/' /boot/cmdline.txt        
+else
+        echo "- The cmdline.txt file was already modified"
+fi
+
+# Hide the login text (it will still be available on tty3 - connect a keyboard to your pi and press CTRl-ALT-F3 to see it)
+systemctl enable getty@tty3.service
+systemctl disable getty@tty1.service
 
 
-# disable Openbox keyboard shortcuts to make the kiosk mode harder to escape
+
+# Sets more power for USB ports
+isInFile3=$(cat /boot/config.txt | grep -c "max_usb_current")
+if [ $isInFile3 -eq 0 ]
+then
+	echo "- Setting USB to deliver more current in config.txt"
+	echo 'max_usb_current=1' >> /boot/config.txt
+else
+    echo "- USB was already set to deliver more current in config.txt"
+fi
+
+
+
+# Disable Openbox keyboard shortcuts to make the kiosk mode harder to escape
 wget https://www.candlesmarthome.com/tools/rc.xml -P /etc/xdg/openbox/rc.xml
+
+# Modify the xinitrc file to automatically log in the pi user
+echo "- Creating xinitrc file"
+echo 'exec openbox-session' > /etc/X11/xinit/xinitrc
+
+echo "- Creating xwrapper.config file"
+echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
+echo "needs_root_rights=yes" >> /etc/X11/Xwrapper.config
+
+
+
+
+
 
 # CHROMIUM 
 
 # Add policy file to disable things like file selection
 mkdir -p /etc/chromium/policies/managed/
 echo '{"AllowFileSelectionDialogs": false, "AudioCaptureAllowed": false}' > /etc/chromium/policies/managed/candle.json
+
 
 
 
