9f719b737e25089aaf965f5036a68686da99cba6
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-05-19 15:49:57 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+
+
+mkdir /home/pi/.webthings/etc/
+
+# move hosts file to user partition
+cp /etc/hosts /home/pi/.webthings/etc/hosts
+rm /etc/hosts
+ln -s /home/pi/.webthings/etc/hosts /etc/hosts
+
+# move timezone file to user partition
+cp /etc/timezone /home/pi/.webthings/etc/timezone
+rm /etc/timezone 
+ln -s /home/pi/.webthings/etc/timezone /etc/timezone 
