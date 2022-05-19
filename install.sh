0ec54e78f1d9be55a25f74c111635b0c95a282b6
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-05-19 16:06:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 # this script assumess that all the files have already been copied into place
 
 
-mkdir /home/pi/.webthings/etc/
+# MAKE DIRECTORIES
+mkdir /home/pi/.webthings/etc
+mkdir /home/pi/.webthings/var/lib
+mkdir /home/pi/.webthings/tmp
+
+mkdir /home/pi/.webthings/arduino/.arduino15
+mkdir /home/pi/.webthings/arduino/Arduino
+
+
 
 # SYMLINKS
 
@@ -16,13 +24,27 @@ rm /etc/timezone
 ln -s /home/pi/.webthings/etc/timezone /etc/timezone 
 
 
+
 # BINDS
 
 cp -r /etc/ssh /home/pi/.webthings/etc/ssh
 cp -r /etc/wpa_supplicant /home/pi/.webthings/etc/wpa_supplicant/
+cp -r /var/lib/bluetooth /home/pi/.webthings/var/lib/bluetooth
+
+
 
 
 # SERVICES
 systemd disable webthings-gateway.check-for-update.service
 systemd disable webthings-gateway.check-for-update.timer
 systemd disable webthings-gateway.update-rollback.service
+
+systemd enable candle_first_run.service
+systemd enable candle_reset.service
+systemd enable candle_start_swap.service
+
+
+
+TODO:
+# build and install BlueAlsa with legaly safe codes and built-in audio mixing
+# directory ownership and permissions
