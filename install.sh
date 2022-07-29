005d0b1207d417c631f932026d0cd467cc038e54
flatsiedatsie <unfold@gmail.com>
2022-07-29 11:52:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # Flash basic Raspberry Pi OS Lite 64 image using Raspberry Pi Imager software. 
 # https://www.raspberrypi.com/software/
 
-# Use the gear icon to set everything up that you can: enable ssh (username "pi", and password "smarthome"), set the hostname to "candle", pre-fill your wifi credentials, etc.
+# Use the gear icon to set everything up that you can: enable ssh, set username "pi" and password "smarthome", set the hostname to "candle", pre-fill your wifi credentials, etc.
 
 # Once flashing is complete, unplug the SD card from your computer and re-insert it into your computer. A new disk called "boot" should appear. Edit the file called “cmdline.txt”. From it, remove “init=/usr/lib/raspi-config/init_resize.sh”, and save. Saving might seem to fail, but it probably saved anyway.
 
