6572e74cbdf49eecb07ea60d090234993618ff20
flatsiedatsie <unfold@gmail.com>
2022-08-01 08:56:34 +0200

    Update install_candle_controller.sh

--- install_candle_controller.sh
+++ install_candle_controller.sh
@@ -4,9 +4,12 @@
 
 # Check if script is being run as root
 if [ "$EUID" -ne 0 ]
-  
-    echo "HOME: $HOME"
-  
+then
+
+  echo "HOME: $HOME"
+
+  cd /home/pi
+
   python3 -m pip install git+https://github.com/WebThingsIO/gateway-addon-python#egg=gateway_addon
 
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
@@ -25,9 +28,11 @@ if [ "$EUID" -ne 0 ]
   echo "DOWNLOADING AND INSTALLING CANDLE CONTROLLER"
   
   # Download Candle controller from Github and install it
-  mkdir /home/pi/webthings
+  mkdir -p /home/pi/webthings
   chown pi:pi /home/pi/webthings
   cd /home/pi/webthings
+  rm -rf candle-controller
+  rm -rf gateway
   git clone --depth 1 https://github.com/createcandle/candle-controller.git
   mv candle-controller gateway
   chown -R pi:pi /home/pi/webthings/gateway
@@ -41,8 +46,37 @@ if [ "$EUID" -ne 0 ]
   # npm install
   CPPFLAGS="-DPNG_ARM_NEON_OPT=0" npm ci
   
+  echo " "
+  echo "COMPILING TYPESCRIPT AND RUNNING WEBPACK"
+  #npm run build
+  
+  npm install -D webpack-cli
+  
+  rm -rf build
+  cp -rL src build
+  cp -rL static build/static
+  find build -name '*.ts' -delete
+  npx tsc -p .
+  npx webpack
+  
+  echo " "
+  echo "INSTALLING CANDLE STORE ADDON"
+  echo " "
+
+  cd /home/pi/.webthings/addons
+  rm candleappstore-0.4.17-linux-arm64-v3.9.tgz
+  rm -rf package
+  rm -rf candleappstore
+  wget https://github.com/createcandle/candleappstore/releases/download/0.4.17/candleappstore-0.4.17-linux-arm64-v3.9.tgz
+  tar -xf candleappstore-0.4.17-linux-arm64-v3.9.tgz
+  mv package candleappstore
+  chown pi:pi candleappstore
+  rm candleappstore-0.4.17-linux-arm64-v3.9.tgz
+  
+  timeout 5 npm run run-only
+  
 else
-  then echo "Please do not run as root"
+  echo "Please do not run as root"
   exit
 fi
 
