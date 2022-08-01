d322de151ed08a130a4ac482289f563ab214f310
flatsiedatsie <unfold@gmail.com>
2022-08-01 10:46:44 +0200

    Update create_candle_disk_image.sh

--- create_candle_disk_image.sh
+++ create_candle_disk_image.sh
@@ -385,13 +385,13 @@ echo '{"AllowFileSelectionDialogs": false, "AudioCaptureAllowed": false}' > /etc
 
 # ADD IP-TABLES
 
-  echo "Redirecting :80 to :8080 and :443 to :4443"
-  iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
-  iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
-  iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-  iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
-  iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
-  iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
+echo "Redirecting :80 to :8080 and :443 to :4443"
+iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
+iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
 
 
 
@@ -399,9 +399,12 @@ echo '{"AllowFileSelectionDialogs": false, "AudioCaptureAllowed": false}' > /etc
 
 # ~/.config/configstore/update-notifier-npm.json <- set output to true
 # Respeaker drivers?
+
 # update python libraries except for 2 (schemajson and... )
-
-
+# pip3 list --outdated
+# not to be updated:
+# - websocket-client
+# - jsonschema (if memory serves)
 
 # RASPI CONFIG
 
@@ -425,4 +428,6 @@ raspi-config nonint do_camera 0
 # disable swap file
 dphys-swapfile swapoff
 
-
+echo " "
+echo "DONE! Reboot the controller."
+echo " "
