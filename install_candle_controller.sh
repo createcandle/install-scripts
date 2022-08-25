51bc4c62b8ba225939f20e8b8a620c3ce493b28a
flatsiedatsie <unfold@gmail.com>
2022-08-25 18:55:54 +0200

    Add files via upload

--- install_candle_controller.sh
+++ install_candle_controller.sh
@@ -427,18 +427,18 @@ if [ -d /home/pi/webthings/gateway2 ]; then
     if [ "$totalk" -lt 600000 ]
     then
         echo "very low memory, --max-old-space-size=496"
-        NODE_OPTIONS="--max-old-space-size=496" npx webpack --yes
+        NODE_OPTIONS="--max-old-space-size=496" npx webpack
     elif [ "$totalk" -lt 1200000 ]
     then
         echo "low memory, --max-old-space-size=750"
-        NODE_OPTIONS="--max-old-space-size=750" npx webpack --yes
+        NODE_OPTIONS="--max-old-space-size=750" npx webpack
     elif [ "$totalk" -lt 2200000 ]
     then
         echo "normal memory, --max-old-space-size=1024"
-        NODE_OPTIONS="--max-old-space-size=1024" npx webpack --yes
+        NODE_OPTIONS="--max-old-space-size=1024" npx webpack  #--yes
     else
         echo "big memory, --max-old-space-size=2048"
-        NODE_OPTIONS="--max-old-space-size=2048" npx webpack --yes
+        NODE_OPTIONS="--max-old-space-size=2048" npx webpack
     fi
     
     if [ -f /home/pi/webthings/gateway2/build/app.js ] \
