4c9c5a27eff51790845d5669ab2f2c737c4de164
flatsiedatsie <unfold@gmail.com>
2023-01-20 16:35:58 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -2389,11 +2389,13 @@ if [ ! -f "/boot/splash_updating-0.png" ]; then
     wget https://www.candlesmarthome.com/tools/splash_updating-2.png -O /boot/splash_updating-2.png
     wget https://www.candlesmarthome.com/tools/splash_updating-3.png -O /boot/splash_updating-3.png
     wget https://www.candlesmarthome.com/tools/splash_updating-4.png -O /boot/splash_updating-4.png
+    wget https://www.candlesmarthome.com/tools/splash_updating-5.png -O /boot/splash_updating-5.png
     wget https://www.candlesmarthome.com/tools/splash_updating180-0.png -O /boot/splash_updating180-0.png
     wget https://www.candlesmarthome.com/tools/splash_updating180-1.png -O /boot/splash_updating180-1.png
     wget https://www.candlesmarthome.com/tools/splash_updating180-2.png -O /boot/splash_updating180-2.png
     wget https://www.candlesmarthome.com/tools/splash_updating180-3.png -O /boot/splash_updating180-3.png
     wget https://www.candlesmarthome.com/tools/splash_updating180-4.png -O /boot/splash_updating180-4.png
+    wget https://www.candlesmarthome.com/tools/splash_updating180-5.png -O /boot/splash_updating180-5.png
     wget https://www.candlesmarthome.com/tools/error.png -O /boot/error.png
 fi
 
