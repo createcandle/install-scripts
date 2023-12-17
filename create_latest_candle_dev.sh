92e8a7a3e217b32e75e7f93c461473bbb09054f1
flatsiedatsie <unfold@gmail.com>
2023-12-17 19:27:33 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -1572,13 +1572,12 @@ then
     # removed libdbus-glib-1-dev (bluealsa now uses version 2 instead)
     # For bookworm libavcodec58 was changed to libavcodec59
     for i in \
-    $CHROMIUM_PACKAGE_NAME git \
-    autoconf build-essential curl libbluetooth-dev libboost-python-dev libboost-thread-dev libffi-dev \
-        libglib2.0-dev libpng-dev libcap2-bin libudev-dev libusb-1.0-0-dev pkg-config lsof python3-six \
+    git autoconf build-essential curl libbluetooth-dev libboost-python-dev libboost-thread-dev libffi-dev \
+    libglib2.0-dev libpng-dev libcap2-bin libudev-dev libusb-1.0-0-dev pkg-config lsof python3-six \
     arping autoconf ffmpeg libtool mosquitto policykit-1 sqlite3 libolm3 nbtscan ufw iptables \
     liblivemedia-dev libavcodec59 libswresample3 libffi8 libavformat59 \
     libasound2-dev libgirepository1.0-dev libsbc-dev libmp3lame-dev libspandsp-dev \
-    python3-libcamera python3-kms++ python3-prctl libatlas-base-dev libopenjp2-7;
+    python3-libcamera2 python3-kms++ python3-prctl libatlas-base-dev libopenjp2-7;
     do
         echo
         if [ -n "$(dpkg -s $i | grep 'install ok installed')" ]; then
