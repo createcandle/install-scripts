973901424e04e23b4e4f0adc64e1bc3b541c7a8e
flatsiedatsie <unfold@gmail.com>
2023-12-29 23:00:34 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -438,8 +438,9 @@ if [ "$SKIP_DHCPCD" = no ] || [[ -z "${SKIP_DHCPCD}" ]]; then
 	echo "completely removing network manager"
 	rm -rf /etc/NetworkManager/system-connections/*
 	apt purge -y network-manager
-	
-	apt install -y --no-install-recommends dns-root-data dnsmasq libbluetooth3 libndp0 libnetfilter-conntrack3 libnfnetlink0 libteamdctl0 raspberrypi-net-mods iptables
+
+ 	# libteamdctl0 raspberrypi-net-mods
+	apt install -y --no-install-recommends dns-root-data dnsmasq libbluetooth3 libndp0 libnetfilter-conntrack3 libnfnetlink0 iptables
 	
 	ls /etc/dhcp/dhclient-enter-hooks.d/resolvconf
 	
