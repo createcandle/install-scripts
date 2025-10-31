1539d8cfdc01871fea08b9782646d6e0ee2ca067
flatsiedatsie <unfold@gmail.com>
2025-10-31 22:42:25 +0100

    Update create_latest_candle_dev2.sh

--- create_latest_candle_dev2.sh
+++ create_latest_candle_dev2.sh
@@ -1520,7 +1520,7 @@ then
  	apt install -y pipewire-plugin-libcamera --no-install-recommends
 
 	echo "installing some network applications"
-	apt install -y --no-install-recommends dns-root-data dnsmasq libbluetooth3 libndp0 libnetfilter-conntrack3 libnfnetlink0 iptables dbus-launch libdbus-1-dev libssl-dev
+	apt install -y --no-install-recommends dns-root-data dnsmasq libbluetooth3 libndp0 libnetfilter-conntrack3 libnfnetlink0 iptables dbus-x11 libdbus-1-dev libssl-dev
 	systemctl stop dnsmasq.service
 	#systemctl enable dnsmasq.service
 	systemctl disable dnsmasq.service
