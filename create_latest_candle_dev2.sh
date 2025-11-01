c81ec014b98f054832841fe9832b1a4daef62ecf
flatsiedatsie <unfold@gmail.com>
2025-11-01 02:56:17 +0100

    Update create_latest_candle_dev2.sh

--- create_latest_candle_dev2.sh
+++ create_latest_candle_dev2.sh
@@ -1998,6 +1998,7 @@ sudo -u pi python3 -m pip install --upgrade pip
 apt install -y --no-install-recommends python3-dbus python3-cffi python3-gpiozero python3-pil python3-wheel python3-requests python3-distro python3-certifi python3-urllib3 python3-colorzero python3-chardet python3-libevdev python3-numpy python3-bluetooth python3-pillow
 sudo -u pi pip3 uninstall -y adapt-parser || true
 sudo -u pi pip3 install pycryptodomex --break-system-packages
+sudo -u pi pip3 install pillow --break-system-packages
 
 #sudo -u pi pip3 install --break-system-packages git+https://github.com/pybluez/pybluez.git#egg=pybluez
 sudo -u pi pip3 install --break-system-packages git+https://github.com/pybluez/pybluez.git#egg=pybluez 
