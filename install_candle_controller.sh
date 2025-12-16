b56e942d4145b5f69c1d8c5e88d1043eca02b5ac
flatsiedatsie <unfold@gmail.com>
2025-12-16 11:14:45 +0100

    Update install_candle_controller.sh

--- install_candle_controller.sh
+++ install_candle_controller.sh
@@ -545,7 +545,7 @@ if [ -d "$CANDLE_BASE/webthings/gateway2" ]; then
 	git submodule update
 	CPPFLAGS="-DPNG_ARM_NEON_OPT=0" npm --yes i --save
 	node generate-version.js && node generate-types.js && npx tsc -p .
-	
+	ls
 	#cd ../..
 	cd "$CANDLE_BASE/webthings/gateway2"
     
