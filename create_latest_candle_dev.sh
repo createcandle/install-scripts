be579a5f1badae91913fcd97f48a49723ca58def
flatsiedatsie <unfold@gmail.com>
2025-12-24 10:51:07 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -324,7 +324,7 @@ then
             if [[ -z "${TINY_PARTITIONS}" ]]; then
                 echo "normal partition size"
                 echo -e "Yes\nYes" | /usr/sbin/parted /dev/mmcblk0 ---pretend-input-tty --align optimal resizepart 2 8000MB
-                printf "mkpart\np\next4\n8546MB\n16546MB\nmkpart\np\next4\n16548MB\n26000MB\nquit" | /usr/sbin/parted --align optimal
+                printf "mkpart\np\next4\n8746MB\n16546MB\nmkpart\np\next4\n16548MB\n26000MB\nquit" | /usr/sbin/parted --align optimal
 
 				# parted -s --align optimal /dev/sda -- mklabel gpt mkpart primary 4MiB 1 50% mkpart primary 4MiB 50% 100% set 1 boot
 				
