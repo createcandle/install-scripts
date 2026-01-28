207cb7b0af7c52e24ded81a37ed7f8bece3f3ee3
flatsiedatsie <unfold@gmail.com>
2026-01-28 23:05:13 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -1548,7 +1548,7 @@ then
     echo "installing Pipewire audio"
 	# software-properties-common
 	apt install -y rtkit
-    apt install -y pipewire wireplumber pipewire-alsa pipewire-jack pipewire-pulse libspa-0.2-bluetooth pipewire-audio-client-libraries easyeffects portaudio19-dev --no-install-recommends
+    apt install -y pipewire wireplumber pipewire-alsa pipewire-jack pipewire-pulse libspa-0.2-bluetooth pipewire-audio-client-libraries pipewire-libcamera easyeffects portaudio19-dev --no-install-recommends
     raspi-config nonint do_audioconf 2
     apt update -y
 
