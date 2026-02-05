80b2d6fd6d4dd90438a68df1d97cb3374d1a0195
flatsiedatsie <unfold@gmail.com>
2026-02-05 13:55:37 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -633,9 +633,9 @@ apt-get update
 
 # remove Firefox and other applications, if they are installed.
 
-apt remove firefox* galculator* geany* thonny* wolfram-engine* oracle-java* scratch* libreoffice* cloud* netplan --purge -y
-apt remove kanshi eatmydata pocketsphinx* raindrop wayvnc cloud-init cloud-guest-utils aspell* autotouch apparmor docutils-common evince* feedbackd* --purge -y
-apt remove bookshelf --purge -y
+apt remove firefox* galculator* geany* thonny* wolfram-engine* oracle-java* scratch* libreoffice* cloud* netplan --purge -y 1> /dev/null
+apt remove kanshi eatmydata pocketsphinx* raindrop wayvnc cloud-init cloud-guest-utils aspell* autotouch apparmor docutils-common evince* feedbackd* --purge -y 1> /dev/null
+apt remove bookshelf --purge -y 1> /dev/null
 
 # in theory removing ModemManager could prevent issues with zigbee2Mqtt. For now it's just being completely disabled.
 #apt remove modemmanager --purge -y 
