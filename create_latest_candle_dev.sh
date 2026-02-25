107db287d941052d8456f67f64bbe27f72d7a0ed
flatsiedatsie <unfold@gmail.com>
2026-02-25 20:02:53 +0100

    Update create_latest_candle_dev.sh

--- create_latest_candle_dev.sh
+++ create_latest_candle_dev.sh
@@ -632,14 +632,12 @@ apt-get update
 # remove Firefox and other applications, if they are installed.
 
 echo "removing first batch"
-apt remove firefox* galculator* geany* thonny* wolfram-engine* oracle-java* scratch* libreoffice* cloud* netplan* --purge -y 
-#1> /dev/null
+# netplan* 
+apt remove firefox* galculator* geany* thonny* wolfram-engine* oracle-java* scratch* libreoffice* cloud* --purge -y 1> /dev/null
 echo "removing second batch"
-apt remove kanshi eatmydata pocketsphinx* raindrop wayvnc cloud-init cloud-guest-utils aspell* autotouch apparmor* docutils-common evince* feedbackd* --purge -y 
-#1> /dev/null
+apt remove kanshi eatmydata pocketsphinx* raindrop wayvnc cloud-init cloud-guest-utils aspell* autotouch apparmor* docutils-common evince* feedbackd* --purge -y 1> /dev/null
 echo "removing third batch"
-apt remove bookshelf --purge -y 
-#1> /dev/null
+apt remove bookshelf --purge -y 1> /dev/null
 
 apt install 
 
